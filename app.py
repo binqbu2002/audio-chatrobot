d98a734cb0edda5013a0cb7f8026719f324ee386
wangbin <binqbu2002@gmail.com>
2023-05-17 00:08:18 +0100

    git add .

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def post_wav(wav_path):
     #     '"filename': open('audio.wav"', 'rb'),
     # }
 
-    files = {'file': (wav_path, open('audio.wav', 'rb'))}
+    files = {'file': (wav_path, open('audio.mp3', 'rb'))}
 
     response = requests.post('http://18.132.153.117:5000/upload', files=files)
 
@@ -22,9 +22,9 @@ def index():
 
 
 
-@app.route('/audio.wav', methods=['GET', 'POST'])
+@app.route('/audio.mp3', methods=['GET', 'POST'])
 def serve_audio():
-    return send_from_directory(directory='.', path='audio.wav')
+    return send_from_directory(directory='.', path='audio.mp3')
 
 @app.route('/music_play', methods=['GET'])
 def music_play():
@@ -32,38 +32,38 @@ def music_play():
     # return the URL of the music file
     print("ask GPT")
     # os.system('curl -X POST -F \"file=@audio.wav\" http://18.132.153.117:5000/upload')
-    post_wav('audio.wav')
-    if os.path.exists('audio.wav'):
-        os.remove('audio.wav')
+    post_wav('audio.mp3')
+    if os.path.exists('audio.mp3'):
+        os.remove('audio.mp3')
     # os.system('rm -rf ./audio.wav')
     # os.system('curl -OJ http://18.132.153.117:5000/download/audio.wav')
 
-    response = requests.get('http://18.132.153.117:5000/download/audio.wav')
+    response = requests.get('http://18.132.153.117:5000/download/audio.mp3')
 
     music = response.content
 
-    with open(r'audio.wav', 'ab') as file:  # 保存到本地的文件名
+    with open(r'audio.mp3', 'ab') as file:  # 保存到本地的文件名
         file.write(response.content)
         file.flush()
 
     # music_file_path = './audio.wav'
     # output_path = './audio.wav'
     #
-    music_file_path = './audio.wav'
-    output_path = './audio.wav'
+    music_file_path = './audio.mp3'
+    output_path = './audio.mp3'
     #
     # Convert the audio file to 16kHz
     audio = AudioSegment.from_file(music_file_path)
     converted_audio = audio.set_frame_rate(16000)
-    converted_audio.export(output_path, format='wav')
-    url = "./audio.wav"
+    converted_audio.export(output_path, format='mp3')
+    url = "./audio.mp3"
     return jsonify({'url': url})
 
 @app.route('/record', methods=['POST'])
 def record():
     # os.system('rm -rf ./audio.wav')
-    if os.path.exists('audio.wav'):
-        os.remove('audio.wav')
+    if os.path.exists('audio.mp3'):
+        os.remove('audio.mp3')
     start = time.time()
     audio_file = request.files['audio_data']
     file_path = 'recorded_audio.webm'
@@ -71,9 +71,9 @@ def record():
 
     # Convert the webm file to a wav file using pydub
     webm_audio = AudioSegment.from_file(file_path, format='webm')
-    wav_file_path = 'audio.wav'
+    wav_file_path = 'audio.mp3'
     webm_audio = webm_audio.set_frame_rate(16000)
-    webm_audio.export(wav_file_path, format='wav')
+    webm_audio.export(wav_file_path, format='mp3')
 
     # Clean up temporary files
     os.remove(file_path)
